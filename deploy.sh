398c15b52977659d51eb2c953abae27de22abe99
maronghe <loganma0209@gmail.com>
2020-06-08 17:03:53 +0800

    upload archeives content

--- deploy.sh
+++ deploy.sh
@@ -6,7 +6,8 @@ set -e
 printf "\033[0;32mDeploying updates to GitHub...\033[0m\n"
 
 # Build the project.
-hugo # if using a theme, replace with `hugo -t <YOURTHEME>`
+hugo server --theme=pulp --buildDrafts --watch # if using a theme, replace with `hugo -t <YOURTHEME>`
+
 
 # Go To Public folder
 cd public
